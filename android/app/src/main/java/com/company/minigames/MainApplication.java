b45c1f95dee3ec420503dad308fcdc7ed57e0150
Alison Morgan <alison.morgan406@gmail.com>
2018-12-05 14:16:49 -0700

    added pop-up to password reset

--- android/app/src/main/java/com/company/minigames/MainApplication.java
+++ android/app/src/main/java/com/company/minigames/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
    protected List<ReactPackage> getPackages() {
      return Arrays.asList(
        new MainReactPackage(),
+            new RNFSPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNFirebasePackage(),
